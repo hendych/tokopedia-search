324030ccc0bb9428da83cf357fcb89018a8cef98
Hendy Christianto <hendy_ch90@yahoo.com>
2018-10-02 23:54:47 +0700

    Implement output search results and tests

--- SearchTests/Presenter/SearchPresenterTests.swift
+++ SearchTests/Presenter/SearchPresenterTests.swift
@@ -31,4 +31,31 @@ class SearchPresenterTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+
+    func testOutputFoundSearchResult() {
+        let mockData: [[String: Any]] = [
+            ["id": 123, "name": "test"],
+            ["id": 234, "name": "test2"]
+        ]
+        var results = [ProductItem]()
+
+        for data in mockData {
+            guard let item = ProductItem(json: data) else {
+                XCTFail("Failed when creating product item")
+
+                return
+            }
+
+            results.append(item)
+        }
+
+        presenter?.foundSearchResult(results: results, nextPage: 20)
+
+        XCTAssert(mockView?.invokedShowSearchResultsCount == 1,
+                  "Expect showSearchResults called once")
+        XCTAssert(mockView?.invokedShowSearchResultsParameters?.searchResults == results,
+                  "Expect showSearchResults invoked with parameter results")
+        XCTAssert(mockView?.invokedShowSearchResultsParameters?.nextPage == 20,
+                  "Expect showSearchResults invoked with parameter next page 20")
+    }
 }
