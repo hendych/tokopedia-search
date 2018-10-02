1911f009f2e9142a304045d5bb6cf776ac238e31
Hendy Christianto <hendy_ch90@yahoo.com>
2018-10-02 22:28:41 +0700

    Adding entities for search result and tests

--- Search/Classes/Entity/ProductItem.swift
+++ Search/Classes/Entity/ProductItem.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+struct ProductItem {
+    private(set) var id = 0
+    private(set) var name = ""
+    private(set) var uri = ""
+    private(set) var imageUri = ""
+    private(set) var imageUri700 = ""
+    private(set) var price = ""
+    private(set) var priceRange = ""
+
+    init(json: Any?) {
+        guard let json = json as? [String: Any] else {
+            return
+        }
+
+        self.id = json["id"] as? Int ?? 0
+        self.name = json["name"] as? String ?? ""
+        self.uri = json["uri"] as? String ?? ""
+        self.imageUri = json["image_uri"] as? String ?? ""
+        self.imageUri700 = json["image_uri_700"] as? String ?? ""
+        self.price = json["price"] as? String ?? ""
+        self.priceRange = json["price_range"] as? String ?? ""
+    }
+}
---
+++ SearchTests/Entity/ProductItemsTests.swift
@@ -0,0 +1,40 @@
+//
+//  ProductItemsTests.swift
+//  SearchTests
+//
+//  Created by Hendy Christianto on 02/10/18.
+//  Copyright © 2018 Hendy Christianto. All rights reserved.
+//
+
+import XCTest
+@testable import Search
+
+class ProductItemsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testSerializeJsonProductItem() {
+        let productItem = ProductItem(json: ["id": 0,
+                                             "name": "kabel",
+                                             "uri": "abc",
+                                             "image_uri": "bcd",
+                                             "image_uri_700": "ccc",
+                                             "price": "Rp. 1",
+                                             "price_range": "20"])
+
+        XCTAssert(productItem.id == 0, "Expected id is '0'")
+        XCTAssert(productItem.name == "kabel", "Expected name is 'kabel'")
+        XCTAssert(productItem.uri == "abc", "Expected uri is 'abc'")
+        XCTAssert(productItem.imageUri == "bcd", "Expected imageUri is 'bcd'")
+        XCTAssert(productItem.imageUri700 == "ccc", "Expected imageUri700 is 'ccc'")
+        XCTAssert(productItem.price == "Rp. 1", "Expected imageUri is 'Rp. 1'")
+        XCTAssert(productItem.priceRange == "20", "Expected priceRange is '20'")
+    }
+
+}
